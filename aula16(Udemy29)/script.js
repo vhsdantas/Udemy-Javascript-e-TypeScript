f73c0af516073892ccaa6289afe529433543fc72
Victor Henrique da Silva Froes Dantas <henrique24.dantas@gmail.com>
2025-06-11 20:18:53 -0300

    Dia 4 Udemy

---
+++ aula16(Udemy29)/script.js
@@ -0,0 +1,42 @@
+/*             012345678910  
+const nomoe = 'Luiz Otávio'
+console.log(nome[0])
+*/
+
+
+//               0       1        2         3      4
+const alunos = ['Luiz', 'Maria', 'João']; //Luiza, Eduardo
+
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+// alunos.push('Luiza')
+// alunos.push('Eduardo')
+// console.log(alunos.slice(0, -1))
+
+// console.log(alunos[50])
+
+// delete alunos[1]
+// console.log(alunos)
+
+// const removid = alunos.shift() //Remove do começo
+// const removido = alunos.pop() //Remove do fim
+// console.log(alunos)
+
+
+// alunos.unshift('Luiza') //Adiciona no começo
+// alunos.unshift('Fábio')
+
+// alunos.push('Luiza') //Adiciona no fim
+// alunos.push('Fábio')
+
+// alunos[alunos.length] = 'Luiza' //Adiciona no fim
+// alunos[alunos.length] = 'Fábio'
+// alunos[alunos.length] = 'Luana'
+
+// alunos[0] = 'Eduardo' //Altera
+// alunos[3] = 'Luiza' //Adiciona
+
+// console.log(alunos); 
+// console.log(alunos[0]); 
+// console.log(alunos[2]); 
---
+++ aula17(Udemy31)/script.js
@@ -0,0 +1,20 @@
+// function saudacao(nome){
+//  return `Bom dia ${nome}!`
+// }
+// const variavel = saudacao('Luiz')
+// console.log(variavel)
+
+
+// function soma(x, y){
+//     const resultado = x + y
+//     return resultado
+// }
+// console.log(soma(2,2))
+// console.log(soma(3,1))
+// console.log(soma(5,10))
+
+
+const raiz = function(n){
+    return n ** 0.5
+};
+console.log(raiz(9))
