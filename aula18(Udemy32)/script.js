1ac4630fe2e514b04297471be39fc62ba4a56b06
Victor Henrique da Silva Froes Dantas <henrique24.dantas@gmail.com>
2025-06-16 21:13:24 -0300

    Dia 5 Udemy

---
+++ aula18(Udemy32)/script.js
@@ -0,0 +1,6 @@
+// function criaPessoa(nome, sobrenome, idade){
+//     return{nome, sobrenome, idade};
+// };
+
+// const pessoa1 = criaPessoa('Luiz', 'Henrique', 25);
+// console.log(pessoa1.nome);
---
+++ aula19(Udemy33)/scripts.js
@@ -0,0 +1,5 @@
+/*
+Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol) - Valores copiados
+
+Referência (mutável) - array, object, function - Valores passados por referência
+*/
