386f2af0cd2398e0519b9040c78ee96b6e3b9915
Victor Henrique da Silva Froes Dantas <henrique24.dantas@gmail.com>
2025-08-04 21:44:37 -0300

    Dia 12 Udemy

---
+++ aula31(Udemy49)/index.js
@@ -0,0 +1,33 @@
+ const verdadeira = true;
+
+ // Let tem escopo de bloco {... bloco}
+ // Var só tem escopo de função
+
+//  let nome = 'Luiz'; //criando
+//  var nome2 = 'Luiz'; //criando
+
+//  if (verdadeira) {
+//     let nome = 'Otávio'; //criando
+//     var nome2 = 'Rogério'; //redeclarando
+
+//     if (verdadeira) {
+//         let nome = 'Outra coisa'; //criando
+//         var nome2 = 'Ronaldo'; //redeclarando
+//     }
+//  }
+
+//  console.log(nome, nome2);
+
+// function falaOi() {
+    
+
+//     if (verdadeira){
+//         let nome = 'Luiz';
+//         var sobrenome = 'Miranda';
+//     }
+
+//     console.log(sobrenome);
+// }
+
+// falaOi();
+
---
+++ aula34(Udemy52)/index.js
@@ -0,0 +1,10 @@
+// for (let i = 0; i <= 10; i++) {
+//     const par = i % 2 === 0 ? 'par' : 'ímpar';
+//     console.log(i, par);
+// }
+
+const frutas = ['Maçã', 'Pera', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
